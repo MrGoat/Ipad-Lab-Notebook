0293be53fade4ace18f8e626c77c60247f721aaf
Mr Gout <MrGout@Mrs-MacBook-Air.(none)>
2012-03-04 12:06:07 +0800

    Initial Commit

---
+++ Ipad Lab-NoteBook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ipad Lab-NoteBook
+//
+//  Created by Mr Gout on 4/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
